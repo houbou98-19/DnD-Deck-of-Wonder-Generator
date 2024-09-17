b72463967c220463b20935679963cb0fc1795fc1
houssam <houbou98@gmail.com>
2024-09-17 22:02:30 +0200

    Initial DDD CA MVC Commit

---
+++ src/CardGenerator.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CardGenerator.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
